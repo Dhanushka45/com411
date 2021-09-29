20c1cb655a569fad760f735bcd38bc028c01bedb
Dhanushka Weerasinghe <5weerm74@solent.ac.uk>
2021-09-29 19:44:02 +0530

    Added code to display an ascii art robot.

---
+++ Basics/output/ascii_art.py
@@ -0,0 +1,7 @@
+# Display beep
+print("\t\\**  |  **\\")
+print("\t###########")
+print("#       B         #")
+print("#       E         #")
+print("#       E         #")
+print("#       P         #")
