731294158c1e16de26099831a8c9e3f895d03d4d
Dhanushka Weerasinghe <5weerm74@solent.ac.uk>
2021-10-07 20:59:11 +0530

    Added code to demonstrate the use of a modulus operator

---
+++ Basics/Decisions/Simple_Decisions/modulo_operator.py
@@ -0,0 +1,12 @@
+# Determining if number is odd or even
+
+print("Please enter a number")
+
+number = int(input())
+
+if number % 2 == 0:
+    print(f"number {number} is an even number")
+else:
+    print(f"number {number} is an odd number")
+
+
