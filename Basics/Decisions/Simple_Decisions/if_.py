a1c0ff70841351d9c13ffa12ba7f4be10fa4222a
Dhanushka Weerasinghe <5weerm74@solent.ac.uk>
2021-10-07 20:10:24 +0530

    Added code to demonstrate the use of an if statement.

---
+++ Basics/Decisions/Simple_Decisions/if_.py
@@ -0,0 +1,13 @@
+# Sorting out types of books
+
+print("What type of book is this?")
+
+book = str(input())
+
+if book == "adventure":
+    print("I love adventure books!!")
+
+print("Finished reading book!")
+
+
+
